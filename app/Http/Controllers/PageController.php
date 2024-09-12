c9190bc5463495f2fa963b714a374a04b762911a
Francesco <francesco.dente2003@gmail.com>
2024-09-12 15:19:57 +0200

    1.0.1: build: db created, controller and model created, db not working

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    public function index(){
+        $movies = Movie::all();
+        $title = 'elenco film';
+        return view('home', compact('movies', 'title'));
+    }
+}
